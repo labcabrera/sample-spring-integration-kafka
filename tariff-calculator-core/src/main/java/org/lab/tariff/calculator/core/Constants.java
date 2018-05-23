10770297b872b969800f73d95c6d1e28f3f3a42f
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-05-23 12:34:37 +0100

    Constants

---
+++ tariff-calculator-core/src/main/java/org/lab/tariff/calculator/core/Constants.java
@@ -0,0 +1,14 @@
+package org.lab.tariff.calculator.core;
+
+public interface Constants {
+
+	public interface Topics {
+		String CalculationIn = "tf-calculator-in";
+		String CalculationOut = "tf-calculator-out";
+	}
+
+	public interface MessageKeys {
+		String CalculationMessageKey = "tf-calculator-message-key";
+	}
+
+}
