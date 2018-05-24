7ca85d2cf854ad8b6ec898b4a329178edde3aba8
lab.cabrera@gmail.com <lab.cabrera@gmail.com>
2018-05-24 18:14:42 +0100

    Core version 1.0.0

--- tariff-calculator-core/src/test/java/org/lab/tariff/calculator/core/ApplicationTests.java
+++ tariff-calculator-core/src/test/java/org/lab/tariff/calculator/core/ApplicationTests.java
@@ -1,10 +1,12 @@
 package org.lab.tariff.calculator.core;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Ignore("Not a real unit test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
